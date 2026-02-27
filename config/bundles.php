e1fe93c7e34c8548ecb48cc16fee00e5c6470893
Hicham <72266646+HichamBJM@users.noreply.github.com>
2026-02-27 14:49:19 +0100

    Set demo_admin as admin login account for demo flow

--- config/bundles.php
+++ config/bundles.php
@@ -1,16 +1,27 @@
 <?php
 
-return [
+$bundles = [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
+
+if (class_exists('Symfony\\Bundle\\DebugBundle\\DebugBundle')) {
+    $bundles[Symfony\Bundle\DebugBundle\DebugBundle::class] = ['dev' => true];
+}
+
+if (class_exists('Symfony\\Bundle\\WebProfilerBundle\\WebProfilerBundle')) {
+    $bundles[Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class] = ['dev' => true, 'test' => true];
+}
+
+if (class_exists('Symfony\\Bundle\\MakerBundle\\MakerBundle')) {
+    $bundles[Symfony\Bundle\MakerBundle\MakerBundle::class] = ['dev' => true];
+}
+
+return $bundles;
